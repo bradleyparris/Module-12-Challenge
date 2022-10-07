9982f6db627b404385de8a369c491257ab8b7f8b
Bradley Parris <parrisbjp@gmail.com>
2022-10-07 10:24:10 -0400

    server.js mysql connection and inquirer user prompt

---
+++ server.js
@@ -0,0 +1,107 @@
+const mysql2 = require("mysql2");
+const inquirer = require("inquirer");
+require("console.table");
+
+// Connect to mysql
+const connection = mysql2.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+    user: 'root',
+
+    password: '12345',
+    database: 'employeesDB'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(`
+    ╔═══╗─────╔╗──────────────╔═╗╔═╗
+    ║╔══╝─────║║──────────────║║╚╝║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+    ───────║║──────╔═╝║─────────────────────╔═╝║
+    ───────╚╝──────╚══╝─────────────────────╚══╝`)
+    userPrompt();
+});
+
+// Prompt user
+function userPrompt() {
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "task",
+        message: "Would you like to do?",
+        choices: [
+          "View Employees",
+          "View Employees by Department",
+          "Add Employee",
+          "Remove Employees",
+          "Update Employee Role",
+          "Add Role",
+          "End"]
+      })
+      .then(function ({ task }) {
+        switch (task) {
+          case "View Employees":
+            viewEmployees();
+            break;
+  
+          case "View Employees by Department":
+            viewEmployeeByDepartment();
+            break;
+        
+          case "Add Employee":
+            addEmployee();
+            break;
+  
+          case "Remove Employees":
+            removeEmployees();
+            break;
+  
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+  
+          case "Add Role":
+            addRole();
+            break;
+  
+          case "End":
+            connection.end();
+            break;
+        }
+      });
+  }
+
+
+
+// View employees
+function viewEmployees() {
+    console.log("Viewing employees\n");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log("Employees viewed!\n");
+  
+      userPrompt();
+    });
+  
+  }
